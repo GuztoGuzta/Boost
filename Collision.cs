b2e2d995a50b7f0d40a323c4feaccd41565a63d4
GuztoGuzta <nantapat.v62@rsu.ac.th>
2022-06-01 01:58:43 +0700

    Update Collision.cs

--- Collision.cs
+++ Collision.cs
@@ -6,7 +6,8 @@ public class Collision : MonoBehaviour
 {
 
     [SerializeField] float CrashDelay = 2f;
-    
+    [SerializeField] float NextLevelDelay = 1f;
+
     bool isTransitioning = false;
     // Start is called before the first frame update
     void Start()
@@ -17,7 +18,19 @@ public class Collision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RespondtoDebugKeys();
+    }
 
+    void RespondtoDebugKeys()
+    {
+        if(Input.GetKey(KeyCode.L))
+        {
+            NextLevel();
+        }
+        else if(Input.GetKeyDown(KeyCode.C))
+        {
+            collisionDisable = !collisionDisable;
+        }
     }
 
     private void OnCollisionEnter(Collision other) 
@@ -30,9 +43,6 @@ public class Collision : MonoBehaviour
             case "Finish":
                 Debug.Log("Ayy you landed");
                 Startnextlevel();
-                break;
-            case "Fuel":
-                Debug.Log("Fuel picked up");
                 break;
             default:
                 Debug.Log("You blew it");
@@ -48,9 +58,27 @@ public class Collision : MonoBehaviour
         Invoke("ReloadLevel", CrashDelay);
     }
 
+    void Startnextlevel()
+    {
+        isTransitioning = true;
+        GetComponent<Movement>().enabled = false;
+        Invoke("NextLevel", NextLevelDelay);
+    }
+
     void ReloadLevel()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void NextLevel()
+    {    
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex +1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
